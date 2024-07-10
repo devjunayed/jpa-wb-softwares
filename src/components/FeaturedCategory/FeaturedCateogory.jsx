9fb52a17783d2e32e9af66e227e6b57a9f910903
Md Junayed <devjunayed@gmail.com>
2024-07-10 09:53:15 +0600

    featurd category responsive fix

--- src/components/FeaturedCategory/FeaturedCateogory.jsx
+++ src/components/FeaturedCategory/FeaturedCateogory.jsx
@@ -9,7 +9,7 @@ const FeaturedCateogory = () => {
         <h1 className="font-bold text-xl ">Featured Category</h1>
         <p>Get Your Desired Product from Featured Category!</p>
       </div>
-      <div className="xl:grid-cols-8 grid gap-4 lg:grid-cols-6 grid-cols-4">
+      <div className="xl:grid-cols-8 grid gap-4 lg:grid-cols-6 md:grid-cols-4 grid-cols-2">
         <CategoryCard icon={<TbAirConditioning />} title="AC" />
         <CategoryCard icon={<TbAirConditioning />} title="AC" />
         <CategoryCard icon={<TbAirConditioning />} title="AC" />
